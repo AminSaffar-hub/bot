e461bc4c74d6c708ff9076958a9a926bd3dff2e9
Ismail <ismail_hamrouni@yahoo.fr>
2020-07-28 16:02:20 +0200

    Jetson_files

---
+++ bot_real/bot_interface/include/bot_interface/bot_hw_interface.h
@@ -0,0 +1,94 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, University of Colorado, Boulder
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Univ of CO, Boulder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Dave Coleman
+   Desc:   Example ros_control hardware interface blank template for the bot
+           For a more detailed simulation example, see sim_hw_interface.h
+*/
+
+#ifndef bot_CONTROL__bot_HW_INTERFACE_H
+#define bot_CONTROL__bot_HW_INTERFACE_H
+
+#include <bot_interface/generic_hw_interface.h>
+
+// Publish Subscribe Includes
+#include "std_msgs/Float32MultiArray.h"
+#include "std_msgs/Int64MultiArray.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/MultiArrayLayout.h"
+
+namespace bot_control
+{
+
+
+
+static const double CMD_TO_SET_FACTOR = 0.055;
+/// \brief Hardware interface for a robot
+class botHWInterface : public ros_control_boilerplate::GenericHWInterface
+{
+public:
+  /**
+   * \brief Constructor
+   * \param nh - Node handle for topics.
+   */
+  botHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model = NULL);
+
+  /** \brief Read the state from the robot hardware. */
+  virtual void read(ros::Duration &elapsed_time);
+
+  /** \brief Write the command to the robot hardware. */
+  virtual void write(ros::Duration &elapsed_time);
+
+  /** \breif Enforce limits for all values before writing */
+  virtual void enforceLimits(ros::Duration &period);
+
+  // Subscriber wheel encoder Callback
+  void read_vel(const std_msgs::Int64MultiArray::ConstPtr &vel_msg);
+  
+
+protected:
+  // Publish and Subscriber to from Arduino
+  ros::Publisher wheel_vel_pub;
+  ros::Subscriber wheel_vel_sub;
+  std_msgs::Float32MultiArray vel_setpoint_cmd_array;
+  // To hold the encoder ticks and the previous encoder ticks
+  // Used to calculate speed
+  int curr_vel[4] = {0, 0, 0, 0};
+  double vel_set_cmd[4] = {0., 0., 0., 0.};
+
+};  // class
+
+}  // namespace
+
+#endif
