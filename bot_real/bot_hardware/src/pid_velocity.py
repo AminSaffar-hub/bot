bf8c8ed1ff85f9ed9f4ef4c7b7c6a15f052bd64d
Ismail <ismail_hamrouni@yahoo.fr>
2020-08-26 12:27:34 +0200

    PID and Arduino Updates ;)

--- bot_real/bot_hardware/src/pid_velocity.py
+++ bot_real/bot_hardware/src/pid_velocity.py
@@ -9,9 +9,9 @@ class PID_Control:
             obj.append(0.0)
     
     def __init__(self):
-        self._KP = 0.2
+        self._KP = 0.02
         self._KD = 0.0
-        self._KI = 0.0
+        self._KI = 0.0005
 
         self._Vc = []
         self._Vpos = []
@@ -32,7 +32,11 @@ class PID_Control:
         self.appending_variables(self._sum_error)
         self.appending_variables(self._diff_error)
         self.appending_variables(self._cmd)
-        self.appending_variables(self._pwm_base)
+        #self.appending_variables(self._pwm_base)
+        self._pwm_base.append(80.0)
+        self._pwm_base.append(80.0)
+        self._pwm_base.append(80.0)
+        self._pwm_base.append(80.0)
         self.appending_variables(self._pwm)
 
         rospy.init_node('pwm_controller', anonymous=True)
@@ -61,15 +65,18 @@ class PID_Control:
     
     
     def set_in_interval(self):
-
+        self._cmd[2] *= 2.5 
+        self._cmd[3] *= 2.5
         for i in range(0,4) :
-            self._pwm_base[i] = self._pwm_base[i] + self._cmd[i]
+           # if (i == 2) or (i == 3): self._cmd += 30 
+            self._pwm_base[i] =  self._pwm_base[i] + self._cmd[i]
             
             if self._pwm_base[i] > 255.0 :
                 self._pwm_base[i] = 255.0 
             elif self._pwm_base[i] < -255.0 :
                 self._pwm_base[i] = -255.0 
-            
+
+
             
     
 
@@ -83,7 +90,7 @@ class PID_Control:
             self._prev_error[i] = self._error[i]
             
             self._cmd[i] = self._KP *self._error[i] + self._KI *self._error[i] + self._KD *self._error[i]
-            #rospy.loginfo(self._error)
+            rospy.loginfo(self._error)
             
             # self._pwm_pub.data[1] = 0.0
             # self._pwm_pub.data[2] = 0.0
@@ -93,12 +100,16 @@ class PID_Control:
             self._pwm_pub.data[1] = 0.0
             self._pwm_pub.data[2] = 0.0
             self._pwm_pub.data[3] = 0.0
+            self._pwm_base[0] = 80.0
+            self._pwm_base[1] = 80.0
+            self._pwm_base[2] = 80.0
+            self._pwm_base[3] = 80.0
         else :
             self.set_in_interval()
-            self._pwm_pub.data[0] = self._pwm_base[0]
-            self._pwm_pub.data[1] = self._pwm_base[1]
-            self._pwm_pub.data[2] = self._pwm_base[2]
-            self._pwm_pub.data[3] = self._pwm_base[3]
+            self._pwm_pub.data[0] = self._pwm_base[0] # left 
+            self._pwm_pub.data[1] = self._pwm_base[2] # right 
+            self._pwm_pub.data[2] = self._pwm_base[3] # right B
+            self._pwm_pub.data[3] = self._pwm_base[1] # left B
 
         self.pub.publish(self._pwm_pub)
     
--- bot_real/velocity_node/velocity_node.ino
+++ bot_real/velocity_node/velocity_node.ino
@@ -2,19 +2,15 @@
 #include <ros.h>
 #include <std_msgs/Float32MultiArray.h>
 
-#define KP 1
-#define KI 1
-#define KD 0
-
 #define enableM1 5
 #define enableM2 6
 #define enableM3 10
 #define enableM4 11
 
-#define MLF1 4
-#define MLF2 2
-#define MRF1 8
-#define MRF2 7
+#define MLF1 8
+#define MLF2 7
+#define MRF1 4
+#define MRF2 2
 
 #define MLB1 A0
 #define MLB2 A1
@@ -23,10 +19,7 @@
 
 #define num_motors 4
 
-float prev_error[num_motors];
-float error[num_motors];
-float sum_error[num_motors];
-float diff_error[num_motors];
+
 float cmd[num_motors];
 float* p;
 int pwm_base[num_motors];
@@ -35,75 +28,24 @@ float Vpos[num_motors];
 int flag_cmd = 0;
 
 
-float* set_in_interval(float* uin)
-{
- for (int flag = 0; flag < num_motors ; flag++)
- {
-    pwm_base[flag] += uin[flag];
-    
-    if (pwm_base[flag] > 255) cmd[flag] = 255;
-    else if (pwm_base[flag] < 20) cmd[flag] = 20;
-    else cmd[flag] = pwm_base[flag];
- }
-  return(cmd);
-}
-
 void set_pwm(float* pwm)
 {
-  analogWrite(enableM3,(int)abs(pwm[0]));
-  if (pwm[0]> 0) { digitalWrite(MLF1,HIGH);digitalWrite(MLF2,LOW);}
+  analogWrite(enableM4,(int)abs(pwm[0]));
+  if (pwm[0]>= 0) { digitalWrite(MLF1,HIGH);digitalWrite(MLF2,LOW);}
   else { digitalWrite(MLF1,LOW);digitalWrite(MLF2,HIGH);}
-  analogWrite(enableM4,(int)abs(pwm[1]));
-  if (pwm[1]> 0) { digitalWrite(MRF1,HIGH);digitalWrite(MRF2,LOW);}
+  analogWrite(enableM3,(int)abs(pwm[1]));
+  if (pwm[1]>= 0) { digitalWrite(MRF1,HIGH);digitalWrite(MRF2,LOW);}
   else { digitalWrite(MRF1,LOW);digitalWrite(MRF2,HIGH);}
-  analogWrite(enableM1,(int)abs(pwm[2]));
-  if (pwm[2]> 0) { digitalWrite(MLB1,HIGH);digitalWrite(MLB2,LOW);}
+  analogWrite(enableM1,(int)abs(pwm[3]));
+  if (pwm[3]>= 0) { digitalWrite(MLB1,HIGH);digitalWrite(MLB2,LOW);}
   else { digitalWrite(MLB1,LOW);digitalWrite(MLB2,HIGH);}
-  analogWrite(enableM2,(int)abs(pwm[3]));//(int)pwm[1]);
-  if (pwm[3]> 0) { digitalWrite(MRB1,HIGH);digitalWrite(MRB2,LOW);}
+  analogWrite(enableM2,(int)abs(pwm[2]));//(int)pwm[1]);
+  if (pwm[2]>= 0) { digitalWrite(MRB1,HIGH);digitalWrite(MRB2,LOW);}
   else { digitalWrite(MRB1,LOW);digitalWrite(MRB2,HIGH);}
   delay(5);
 }
 
-void set_pwm_manuel()
-{
-  analogWrite(enableM3,150);//(int)pwm[0]);
-  analogWrite(enableM4,150);//(int)pwm[1]);
-  digitalWrite(MLF1,HIGH);
-  digitalWrite(MLF2,LOW);
-  digitalWrite(MRF1,HIGH);
-  digitalWrite(MRF2,LOW);
-
-  analogWrite(enableM1,150);//(int)pwm[0]);
-  analogWrite(enableM2,200);//(int)pwm[1]);
-  digitalWrite(MLB1,HIGH);
-  digitalWrite(MLB2,LOW);
-  digitalWrite(MRB1,HIGH);
-  digitalWrite(MRB2,LOW);
-  delay(5);
-}
-
-void velocity_control()
-{
-
-  for (int flag = 0; flag < num_motors ; flag++)
-  {
-   error[flag] = Vc[flag]-Vpos[flag];
-   sum_error[flag] += error[flag];
-   diff_error[flag] = error[flag] - prev_error[flag];
-   prev_error[flag] = error[flag];
-
-   cmd[flag] = KP * error[flag] + KI * sum_error[flag] + KD * diff_error[flag] ;
-  }
-   
- p = set_in_interval(cmd);
- set_pwm(p);
- 
-}
-
 ros::NodeHandle nh;
-std_msgs::Float32MultiArray ss;
-ros::Publisher chatter("chatter", &ss);
 float set_vel_zeros[num_motors]={0,0,0,0};
 
 void message_cmd (std_msgs::Float32MultiArray table ){  
@@ -115,32 +57,25 @@ void message_Vpos (std_msgs::Float32MultiArray table_Vpos ){
 }
 
 ros::Subscriber<std_msgs::Float32MultiArray> sub_cmd("pwm_cmd", &message_cmd );
-ros::Subscriber<std_msgs::Float32MultiArray> sub_Vpos("vel_pos", &message_Vpos );
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
-  pinMode(MLF1,OUTPUT);
-  pinMode(MLF2,OUTPUT);
-  pinMode(MRF1,OUTPUT);
-  pinMode(MRF2,OUTPUT);
-  pinMode(MLB1,OUTPUT);
-  pinMode(MLB2,OUTPUT);
-  pinMode(MRB1,OUTPUT);
-  pinMode(MRB2,OUTPUT);
+  pinMode(MLF1,OUTPUT);pinMode(MLF2,OUTPUT);
+  pinMode(MRF1,OUTPUT);pinMode(MRF2,OUTPUT);
+  pinMode(MLB1,OUTPUT);pinMode(MLB2,OUTPUT);
+  pinMode(MRB1,OUTPUT);pinMode(MRB2,OUTPUT);
   nh.initNode();
   nh.subscribe(sub_cmd);
-  nh.subscribe(sub_Vpos);
-  nh.advertise(chatter);
+
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   nh.spinOnce();
-  //set_pwm_manuel();
-  if (flag_cmd == 1) set_pwm(Vc);
-  else set_pwm(set_vel_zeros);
-  //set_pwm(set_vel_zeros);
-  //chatter.publish( &ss );
+if (flag_cmd == 1) set_pwm(Vc);
+else set_pwm(set_vel_zeros);
+  
   delay(1);
 }
